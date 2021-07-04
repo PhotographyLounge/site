626714bafe533e1fa8d901005e0aa147b9025138
RealVagabond <48355895+RealVagabond@users.noreply.github.com>
2021-07-04 09:20:19 +0200

    🔖  V2.1 Update

---
+++ site/components/ThemeSwitcher.vue
@@ -0,0 +1,75 @@
+<template>
+	<button @click="setCurrentTheme">
+		<span>
+			<MoonIcon
+				class="icon"
+				:class="$colorMode.preference === 'dark' ? 'show' : 'hide'"
+			/>
+			<SystemIcon
+				class="icon"
+				:class="$colorMode.preference === 'system' ? 'show' : 'hide'"
+			/>
+			<SunIcon
+				class="icon"
+				:class="$colorMode.preference === 'light' ? 'show' : 'hide'"
+			/>
+		</span>
+	</button>
+</template>
+
+<script>
+import SunIcon from '@/assets/icons/sun.svg?inline';
+import MoonIcon from '@/assets/icons/moon.svg?inline';
+import SystemIcon from '@/assets/icons/system.svg?inline';
+
+export default {
+	components: {
+		SunIcon,
+		MoonIcon,
+		SystemIcon,
+	},
+	data() {
+		return {};
+	},
+	methods: {
+		setCurrentTheme() {
+			this.$colorMode.preference =
+				this.$colorMode.value === 'light' ? 'dark' : 'light';
+		},
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+button {
+	border: none;
+	justify-items: center;
+	background: transparent;
+	width: 1.4em;
+	height: 1.4em;
+	padding: 0;
+	display: flex;
+	cursor: pointer;
+}
+
+.icon {
+	display: block;
+	position: absolute;
+	color: var(--primary-color);
+	width: 1.4em;
+	height: 1.4em;
+
+	&:hover,
+	&:focus {
+		color: var(--hover-color);
+	}
+}
+
+.show {
+	opacity: 1;
+}
+
+.hide {
+	opacity: 0;
+}
+</style>
