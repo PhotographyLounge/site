4170200db6e3e765c28f8cdfffba884c2640c1a1
MattA-Official <MattA-Official@users.noreply.github.com>
2021-07-04 19:09:39 +0000

    deploy: 3bfc96eb1af851a16d0b07c47bc00827c014620c

---
+++ _nuxt/static/1625425778/updates/v2-1/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1625425778",layout:"default",error:a,serverRendered:b,routePath:"\u002Fupdates\u002Fv2-1",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ae5a5ddb"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
