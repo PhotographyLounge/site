738cecab2b28532e05fa05cfbff2226e86d9fc87
vagabondlol <vagabondlol@users.noreply.github.com>
2021-08-31 07:22:34 +0000

    deploy: a03d342c4cb1976aa4af1e65a64ef53e7ae31f68

--- _nuxt/static/1630170774/portfolios/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630170774",layout:"center",error:a,serverRendered:b,routePath:"\u002Fportfolios",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c551125a"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
