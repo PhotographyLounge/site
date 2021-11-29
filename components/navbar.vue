728cd1d937b110b18711fa177566f764622bffe5
Vagabond <48355895+vagabondlol@users.noreply.github.com>
2021-11-29 11:49:14 +0100

    changing link again

--- components/navbar.vue
+++ components/navbar.vue
@@ -3,7 +3,7 @@
 		<nav>
 			<li><NuxtLink to="/">Home</NuxtLink></li>
 			<li><NuxtLink to="/updates">Updates</NuxtLink></li>
-			<li><NuxtLink to="https://snaps.so">Portfolios</NuxtLink></li>
+			<li><NuxtLink to="https://snaps.so">Portfolios</li>
 			<li><NuxtLink to="/staff">Staff</NuxtLink></li>
 		</nav>
 		<ThemeSwitcher />
