ab818a0b0ebd468f26601268caf3472fb23ae5e8
vagabondlol <vagabondlol@users.noreply.github.com>
2021-11-29 10:48:19 +0000

    deploy: 2b8b7d9002885cbd1669f0310109cd7b07365b04

--- _nuxt/static/1634150009/updates/v1-v2/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1634150009",layout:"default",error:a,serverRendered:b,routePath:"\u002Fupdates\u002Fv1-v2",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"1a274ab2"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
