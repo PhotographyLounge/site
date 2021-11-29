8c93f8203be7980d9032f77042ee1313ac5d6fdc
Vagabond <48355895+vagabondlol@users.noreply.github.com>
2021-11-29 15:59:47 +0100

    fixing it

--- components/navbar.vue
+++ components/navbar.vue
@@ -4,7 +4,6 @@
 			<li><NuxtLink to="/">Home</NuxtLink></li>
 			<li><NuxtLink to="/updates">Updates</NuxtLink></li>
 			<li><NuxtLink to="/portfolios">Portfolios</NuxtLink></li>
-			<li><NuxtLink to="https://snaps.so">Portfolios</NuxtLink></li>
 			<li><NuxtLink to="/staff">Staff</NuxtLink></li>
 		</nav>
 		<ThemeSwitcher />
