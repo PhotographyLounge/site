9e3ba4914e24cc064bc2d82084692c077ffa8646
Vagabond <48355895+vagabondlol@users.noreply.github.com>
2021-11-29 15:56:56 +0100

    ugh pls help me matt

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,9 +1,10 @@
 <template>
 	<header>
-		<nav>
+				<nav>
 			<li><NuxtLink to="/">Home</NuxtLink></li>
 			<li><NuxtLink to="/updates">Updates</NuxtLink></li>
-			<li><NuxtLink to="https://snaps.so">Portfolios</li>
+			<li><NuxtLink to="/portfolios">Portfolios</NuxtLink></li>
+			<li><NuxtLink to="https://snaps.so">Portfolios</NuxtLink></li>
 			<li><NuxtLink to="/staff">Staff</NuxtLink></li>
 		</nav>
 		<ThemeSwitcher />
