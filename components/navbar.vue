a720f232fbb305d179b70190a3a58d6166920f23
Vagabond <48355895+vagabondlol@users.noreply.github.com>
2021-11-29 16:17:07 +0100

    fingoos crossed

--- components/navbar.vue
+++ components/navbar.vue
@@ -3,7 +3,7 @@
 				<nav>
 			<li><NuxtLink to="/">Home</NuxtLink></li>
 			<li><NuxtLink to="/updates">Updates</NuxtLink></li>
-			<li><NuxtLink to="/portfolios">Portfolios</NuxtLink></li>
+			<li><NuxtLink to="<a href="https://discord.gg/photography" target="_self" rel="noreferrer">Portfolios</NuxtLink></li>
 			<li><NuxtLink to="/staff">Staff</NuxtLink></li>
 		</nav>
 		<ThemeSwitcher />
