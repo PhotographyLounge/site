fa54eff79c9addf11203d28662868d6d06dff2ed
Matthew Adcock <matthew.n.adcock@gmail.com>
2021-12-03 20:37:36 +0000

    Fix this thing
    @vagabondlol why?

--- components/navbar.vue
+++ components/navbar.vue
@@ -3,7 +3,7 @@
 				<nav>
 			<li><NuxtLink to="/">Home</NuxtLink></li>
 			<li><NuxtLink to="/updates">Updates</NuxtLink></li>
-			<li><NuxtLink to="<a href="https://discord.gg/photography" target="_self" rel="noreferrer">Portfolios</NuxtLink></li>
+			<li><a href="https://snaps.so/" target="_self" rel="noreferrer">Portfolios</a></li>
 			<li><NuxtLink to="/staff">Staff</NuxtLink></li>
 		</nav>
 		<ThemeSwitcher />
